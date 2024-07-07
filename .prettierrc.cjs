44f12c88fb3d5896a6357966bf339b8950f5af36
Zhansaule Telisheva <jansaule97@mail.ru>
2024-07-07 18:40:55 +0500

    chore: add eslint, prettier and husky to the project

---
+++ .prettierrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  "arrowParens": "always",
+  "tabWidth": 2,
+  "trailingComma": "none",
+  "semi": true,
+  "printWidth": 80,
+  "singleQuote": true,
+  "jsxSingleQuote": true,
+  "endOfLine": "lf",
+  "bracketSpacing": true,
+  "bracketSameLine": false,
+  "overrides": [
+    {
+      "files": ["**/*.html"],
+      "options": {
+        "parser": "html",
+        "singleQuote": false
+      }
+    },
+    {
+      "files": ["**/*.scss"],
+      "options": {
+        "parser": "scss",
+        "singleQuote": false
+      }
+    }
+  ]
+};
